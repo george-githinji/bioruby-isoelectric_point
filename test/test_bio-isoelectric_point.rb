8f729b22dad6eb09e3f5fbfd2ea61074ae0b5d99
George <georgkam@gmail.com>
2010-12-24 17:30:25 +0300

    cleanup

--- test/test_bio-isoelectric_point.rb
+++ test/test_bio-isoelectric_point.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class TestBioIsoelectricPoint < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "create a real test" do
+   
   end
 end
