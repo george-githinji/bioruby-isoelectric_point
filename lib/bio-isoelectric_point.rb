28effddd40e965b5cb48269e9df04ee0d0e564d3
George <georgkam@gmail.com>
2011-01-13 22:31:21 +0300

    fixed a typo inthe example code

--- lib/bio-isoelectric_point.rb
+++ lib/bio-isoelectric_point.rb
@@ -5,15 +5,15 @@ require 'bio'
 end
 
 
-	protein_seq = Bio::Sequence::AA.new("KKGFTCGELA")
-
- 	#what is the protein charge at ph 14?
- 	c = charge = protein_seq.charge_at(14)     #=>-2.999795857467562
-
- 	#calculate the pH using dtaselect pka set and round off to 3 decimal places
- 	isoelectric_point = protein_seq.isoelectric_point('dtaselect', 3)   #=>8.219
-  
-  puts c
-  puts isoelectric_point
-
-  
+#   protein_seq = Bio::Sequence::AA.new("KKGFTCGELA")
+#
+# 	#what is the protein charge at ph 14?
+# 	charge = protein_seq.charge_at(14)     #=>-2.999795857467562
+#
+# 	#calculate the pH using dtaselect pka set and round off to 3 decimal places
+# 	isoelectric_point = protein_seq.isoelectric_point('dtaselect', 3)   #=>8.219
+#  
+#   puts charge
+#   puts isoelectric_point
+#
+#  
